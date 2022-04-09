16e4c21e64d1c4565806bc84876cef0186182cab
missozoda <ozoshka1207@gmail.com>
2022-04-09 09:01:32 +0500

    git init .

---
+++ js/script.js
@@ -0,0 +1,12 @@
+alert("Xush kelibsiz!");
+var questionName = prompt("Ismingiz nima?");
+var questionSurName = prompt("Familiyangiz nima?");
+var questionAge = prompt("Necha yoshsiz?");
+var questionJob = prompt("Kim bo`lib ishlaysiz?");
+alert("Ma`lumot uchun rahmat!")
+
+console.log(`${questionName} 
+${questionSurName} 
+${questionAge} 
+${questionJob}
+Ma'lumot uchun rahmat!`);
